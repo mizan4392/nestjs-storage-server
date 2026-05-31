0cf7df8971b61652c7e6a0258fa452339e136c7d
“Mizan” <macbook@macs-MacBook-Pro.local>
2026-05-31 19:05:13 +0200

    fixes v14

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // your Next.js frontend
+    methods: 'GET,POST',
+    credentials: true,
+  });
   app.use('/storage', express.static(path.join(process.cwd(), '/storage')));
   await app.listen(process.env.PORT ?? 9000);
 }
